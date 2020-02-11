83f714575f00217bb3820be25ceb5e9e0ce3ec79
lottemakinen <lottemaria99@gmail.com>
2020-02-11 14:18:39 +0200

    connect feature

--- connect.php
+++ connect.php
@@ -2,9 +2,9 @@
 function connect_db() {
  $servername = "localhost";
  // EDIT HERE YOUR USERNAME AND PASSWORD
- $username = "cards";
- $password = "password";
- $dbname = "cards";
+ $username = "lotte";
+ $password = "lotte";
+ $dbname = "lotte";
  // Create connection
  $conn = new mysqli($servername, $username, $password, $dbname);
  // Check connection
