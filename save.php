8c02d5ac22d500a27d808e781a1cc1eb12ef9703
lottemakinen <lottemaria99@gmail.com>
2020-03-03 14:01:18 +0200

    added save.php

---
+++ save.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+if($_SESSION['username'] == 'username') {
+ include "connect.php";
+ print("<html>");
+ $conn = connect_db();
+ $sql = "SELECT * FROM Addresses";
+ $result = mysqli_query($conn, $sql);
+ if (mysqli_num_rows($result) > 0) {
+ // output data of each row
+ while($row = mysqli_fetch_assoc($result)) {
+ echo $row["id"] . " ";
+ echo $row["name"] . " ";
+ echo $row["street_address"] . " ";
+ echo $row["zip"] . " ";
+ echo $row["city"] . " ";
+print("</html>");
+ mysqli_close($conn);
+}
+?>
+
